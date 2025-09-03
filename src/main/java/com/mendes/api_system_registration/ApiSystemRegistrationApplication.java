2c4532e02cca483339b1f8e5386844067487f40d
Danilo Mendes <danilodev.br@gmail.com>
2025-09-03 19:27:09 -0300

    fisrt commit

---
+++ src/main/java/com/mendes/api_system_registration/ApiSystemRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.mendes.api_system_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSystemRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSystemRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mendes/api_system_registration/ApiSystemRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendes.api_system_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSystemRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
