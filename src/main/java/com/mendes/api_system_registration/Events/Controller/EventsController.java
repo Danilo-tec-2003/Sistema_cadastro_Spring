f4232654daedd7626d0f9dc284f75db769f1d042
Danilo Mendes <danilodev.br@gmail.com>
2025-09-15 16:20:52 -0300

    feature: Update the EventsController with mock data

--- src/main/java/com/mendes/api_system_registration/Events/Controller/EventsController.java
+++ src/main/java/com/mendes/api_system_registration/Events/Controller/EventsController.java
@@ -1,8 +1,34 @@
 package com.mendes.api_system_registration.Events.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-
+@RestController
+@RequestMapping("/events")
 public class EventsController {
+
+    @GetMapping("/all")
+    public String getAllEvents() {
+        return "Retornando todos os Eventos.";
+    }
+
+    @GetMapping("/{id}")
+    public String getEventByEvent() {
+        return "Retornando Evento por ID.";
+    }
+
+    @PostMapping("/create")
+    public String createEvent() {
+        return "Registrando um novo Evento.";
+    }
+
+    @PutMapping("/{id}")
+    public String updateEvent() {
+        return "Alterando o Evento por ID";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteEvent() {
+        return "Deletando o Evento por ID";
+    }
 
 }
