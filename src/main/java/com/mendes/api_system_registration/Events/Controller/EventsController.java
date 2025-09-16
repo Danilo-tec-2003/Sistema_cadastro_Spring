3b565ae8d72cb3bcb7e8d7ffbaa409e75695a66d
Danilo Mendes <danilodev.br@gmail.com>
2025-09-16 16:49:36 -0300

    feature: adding call new method created to fetch all events

--- src/main/java/com/mendes/api_system_registration/Events/Controller/EventsController.java
+++ src/main/java/com/mendes/api_system_registration/Events/Controller/EventsController.java
@@ -1,14 +1,22 @@
 package com.mendes.api_system_registration.Events.Controller;
 
+import com.mendes.api_system_registration.Events.Model.EventsModel;
+import com.mendes.api_system_registration.Events.Service.EventsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/events")
 public class EventsController {
 
+    @Autowired
+    private EventsService eventsService;
+
     @GetMapping("/all")
-    public String getAllEvents() {
-        return "Retornando todos os Eventos.";
+    public List<EventsModel> getAllEvents() {
+        return eventsService.getAllEvents();
     }
 
     @GetMapping("/{id}")
