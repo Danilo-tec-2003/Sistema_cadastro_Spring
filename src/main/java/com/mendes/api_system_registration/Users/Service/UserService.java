7c9b8f7f59003c62f0c363c8218a51c4503a253a
Danilo Mendes <danilodev.br@gmail.com>
2025-09-16 16:22:43 -0300

    feature: add UserService with method to list all users

--- src/main/java/com/mendes/api_system_registration/Users/Service/UserService.java
+++ src/main/java/com/mendes/api_system_registration/Users/Service/UserService.java
@@ -1,4 +1,20 @@
 package com.mendes.api_system_registration.Users.Service;
 
+import com.mendes.api_system_registration.Users.Model.UserModel;
+import com.mendes.api_system_registration.Users.Repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    // Método para listar todos os usuários
+    public List<UserModel> getAllUsers() {
+        return usersRepository.findAll();
+    }
 }
