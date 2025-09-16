bc97e10b36182234be2e364100c9fac795883a2d
Danilo Mendes <danilodev.br@gmail.com>
2025-09-16 16:51:04 -0300

    feature: add method to fetch all events in EventsService

--- src/main/java/com/mendes/api_system_registration/Events/Service/EventsService.java
+++ src/main/java/com/mendes/api_system_registration/Events/Service/EventsService.java
@@ -1,4 +1,20 @@
 package com.mendes.api_system_registration.Events.Service;
 
+
+import com.mendes.api_system_registration.Events.Model.EventsModel;
+import com.mendes.api_system_registration.Events.Repository.EventsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EventsService {
+
+    @Autowired
+    private EventsRepository eventsRepository;
+
+    public List<EventsModel> getAllEvents() {
+       return eventsRepository.findAll();
+    }
 }
