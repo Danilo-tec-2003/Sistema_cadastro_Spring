4b25ffe3d09dcd17db5bb9a097dd6fee5e2704be
Danilo Mendes <danilodev.br@gmail.com>
2025-10-08 22:10:49 -0300

    chore: includes the DTO version through the entity manually in the UserMapper, the paths entity x DTO

--- src/main/java/com/mendes/api_system_registration/Users/DTO/UsersDTO.java
+++ src/main/java/com/mendes/api_system_registration/Users/DTO/UsersDTO.java
@@ -1,4 +1,24 @@
 package com.mendes.api_system_registration.Users.DTO;
 
+import com.mendes.api_system_registration.Events.Model.EventsModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+//DTO
 public class UsersDTO {
+
+    private long id;
+    private String name;
+    private Integer age;
+    private  String city;
+    private String profession;
+    private EventsModel events;
+    private String email;
+
 }
+
--- src/main/java/com/mendes/api_system_registration/Users/Mapper/UsersMapper.java
+++ src/main/java/com/mendes/api_system_registration/Users/Mapper/UsersMapper.java
@@ -1,4 +1,45 @@
 package com.mendes.api_system_registration.Users.Mapper;
 
+import com.mendes.api_system_registration.Users.DTO.UsersDTO;
+import com.mendes.api_system_registration.Users.Model.UserModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UsersMapper {
+
+
+    public UserModel toEntity(UsersDTO usersDTO) {
+        //** mapeando, pega a entidade e trasnforma em um DTO
+        //1 - inicializa o objeto
+        UserModel userModel = new UserModel();
+
+        //2 - pega o atributo do Model e atribui a entidade encapsulada do DTO
+        userModel.setId(usersDTO.getId());
+        userModel.setName(usersDTO.getName());
+        userModel.setAge(usersDTO.getAge());
+        userModel.setCity(usersDTO.getCity());
+        userModel.setProfession(usersDTO.getProfession());
+        userModel.setEvents(usersDTO.getEvents());
+        userModel.setEmail(usersDTO.getEmail());
+
+        return userModel;
+    }
+
+    //** mapeando, pega o DTO e trasnforma em uma entidade
+    public UsersDTO toDTO(UserModel userModel) {
+
+        //1 - inicializa o objeto
+        UsersDTO usersDTO = new UsersDTO();
+
+        //2 - pega as entidades encapsulada do DTO e as trasnforma em entidades
+        usersDTO.setId(userModel.getId());
+        usersDTO.setName(userModel.getName());
+        usersDTO.setAge(userModel.getAge());
+        usersDTO.setCity(userModel.getCity());
+        usersDTO.setProfession(userModel.getProfession());
+        usersDTO.setEvents(userModel.getEvents());
+        usersDTO.setEmail(userModel.getEmail());
+
+        return usersDTO;
+    }
 }
