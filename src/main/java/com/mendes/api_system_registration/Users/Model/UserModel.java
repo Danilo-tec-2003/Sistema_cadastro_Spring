320e80b2efc08601ee8cf95436af999a21ff87bd
Danilo Mendes <danilodev.br@gmail.com>
2025-10-15 23:22:20 -0300

    refactor: refactoring UserModel, adding validations to the main fields and FetchType.LAZY for performance improvement

--- src/main/java/com/mendes/api_system_registration/Users/Model/UserModel.java
+++ src/main/java/com/mendes/api_system_registration/Users/Model/UserModel.java
@@ -11,38 +11,33 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "tb_registration")
+@Table(name = "tb_users")
 public class UserModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
      private Long id;
 
-    @Column(name = "nm_name")
      private String name;
-
-    @Column(name = "nr_age")
      private Integer age;
-
-    @Column(name = "nm_city")
      private  String city;
-
-    @Column(name = "nm_profession")
      private String profession;
 
-     @Column(unique = true)
+     @Column(unique = true, nullable = false) //CPF unico e nao nulo
      private String cpf;
 
      @Column(name = "url_img")
      private String urlImg;
 
+    @Column(unique = true, nullable = false) //email unico e nao nulo
+    private String email;
+
      // Um usuário pertence a apenas um evento
-     @ManyToOne
-     @JoinColumn(name = "events_id") // Foreing Key ou chave estrangeira,  // Define a chave estrangeira que relaciona usuários e eventos
+     @ManyToOne(fetch = FetchType.LAZY) // FetchType.LAZY melhora a performance ao não carregar o evento a menos que seja necessário
+     @JoinColumn(name = "events_id") // Foreing Key,  // Define Foreing Key que relaciona usuários e eventos
      @JsonBackReference //parte “filho” da relação, será ignorada na serialização (evita loop).
      private EventsModel events;
 
-    @Column(name = "email")
-    private String email;
+
 }
 
